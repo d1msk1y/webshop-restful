086945698166764768c754d8ca99fabd45c4ae39
d1msk1y <exect81@gmail.com>
2023-10-19 16:27:08 +0200

    Implement other DB parsing required functionality

--- models/index.ts
+++ models/index.ts
@@ -1,3 +1,5 @@
+import mongoose, {mongo} from "mongoose";
+
 export interface ILogistic {
   MeasureUnit: string;
   ContentUnit: string;
@@ -16,4 +18,47 @@ export interface IProduct {
   Discount: number;
   ProductGroup: string;
   Logistic: ILogistic;
+}
+
+export class Product {
+  public collectionName: string = 'products';
+  public Schema: mongoose.Schema = new mongoose.Schema({
+    ItemNumber: String,
+    SupplierItemNumber: String,
+    SupplierNumber: String,
+    SupplierName: String,
+    DeliveryStatus: String,
+    Price: Number,
+    PriceRange: Number,
+    Discount: Number,
+    ProductGroup: String,
+    Logistic: {
+      MeasureUnit: String,
+      ContentUnit: String,
+      Quantity: Number,
+      MinQuantity: Number
+    }
+  });
+
+  public ProductModel: mongoose.Model<any, {}> = mongoose.model(this.collectionName, this.Schema);
+
+  public mapProductModel(product: IProduct) {
+    return new this.ProductModel({
+      ItemNumber: product.ItemNumber,
+      SupplierItemNumber: product.SupplierItemNumber,
+      SupplierNumber: product.SupplierNumber,
+      SupplierName: product.SupplierName,
+      DeliveryStatus: product.DeliveryStatus,
+      Price: product.Price,
+      PriceRange: product.PriceRange,
+      Discount: product.Discount,
+      ProductGroup: product.ProductGroup,
+      Logistic: {
+        MeasureUnit: product.Logistic.MeasureUnit,
+        ContentUnit: product.Logistic.ContentUnit,
+        Quantity: product.Logistic.Quantity,
+        MinQuantity: product.Logistic.MinQuantity
+      }
+    });
+  }
 }
