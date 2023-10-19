92ffaeedb0c6c2ee00260a51071d9206d4c40672
d1msk1y <exect81@gmail.com>
2023-10-19 14:57:04 +0200

    Add package-lock.json

--- services/db/index.ts
+++ services/db/index.ts
@@ -1,8 +1,6 @@
 import mongoose, {ConnectOptions} from "mongoose";
 import colors from "colors";
 
-
-
 export const initDatabase = async () => {
   const mongoDB = process.env['MONGODB_CONNECT_STRING']
     || colors.bgRed("There is nothing we can do :( (failed to obtain DB connection string)\n@napoleon");
