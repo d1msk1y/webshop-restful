6df7a9b9a2ba996c46b2c29c86e230e5d504c998
d1msk1y <exect81@gmail.com>
2023-10-19 11:22:00 +0200

    Initialize database

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import colors from "colors";
+import {initDatabase} from "./services/db";
 
 const app = express();
 const port = 3000;
@@ -9,4 +11,13 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`App is listening at http://localhost:${port}`);
+});
+
+async function main() {
+    await initDatabase();
+    console.log("Hello World!");
+}
+
+main().then(() => {
+    console.log(colors.bgGreen('Application executed successfully!'));
 });
