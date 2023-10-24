1d8b145ae20144a9fe529bc24158290222d631a4
d1msk1y <exect81@gmail.com>
2023-10-24 12:02:57 +0200

    Filter export data

--- services/fetcher/index.ts
+++ services/fetcher/index.ts
@@ -4,8 +4,8 @@ import {Document} from "mongodb";
 
 
 class DbFetcher {
-  public static async getProducts(Product: Product): Promise<IProduct[]> {
-    const model = await this.getProductModel(Product).find();
+  public static async getProducts(product: Product, filter: {}): Promise<IProduct[]> {
+    const model = await this.getProductModel(product).find(filter);
     if (!model || model.length === 0) {
       console.error(colors.yellow(`No products found`));
       return model;
