f9536397fc60bf244e9a565c3938cfcdcfcfa49a
d1msk1y <exect81@gmail.com>
2023-10-25 14:51:02 +0200

    Style project node

--- components/search/product.ts
+++ components/search/product.ts
@@ -1,16 +1,30 @@
 import {IProduct, Product} from "../../models";
 
 export function renderProduct(product: IProduct): string {
+  const deliveryBadge: string = product.DeliveryStatus === '0' ? 'badge-danger' : 'badge-success';
+  const deliveryStatus: string = product.DeliveryStatus === '0' ? 'nicht' : '';
+
   return `
-    <div class="card mb-3 product">
-        <div class="card-body">
-            <h5 class="card-title product-title">${product.ItemNumber}</h5>
-            <div class="prices">
-                <p class="card-text">
-                    <span class="original-price">Preis: ${product.Price}</span>
-                    <br>
-                    <span>Preis - Rabatt: $${product.Price - (product.Price * (product.Discount / 100))}</span>
-                </p>
+    <div class="card mb-3 product" style="width: 375px; height: 170px; overflow: hidden;">
+        <div class="row no-gutters">
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${product.ItemNumber}</h5>
+                    <p class="card-text"><small class="text-muted">${product.SupplierName || 'Brand not available'}</small></p>
+                </div>
+            </div>
+            <div class="col-md-4 align-self-center text-center" style="position: relative;">
+                <span class="badge ${deliveryBadge}" style="position: absolute; right: 15px; bottom: 15px">Ist ${deliveryStatus} geliefert</span>
+            </div>
+        </div>
+        <div class="card-footer">
+            <div class="row">
+                <div class="col">
+                    <strong>Preis:</strong> </br> ${product.Price}.-
+                </div>
+                <div class="col">
+                    <strong>Rabattpreis:</strong> </br> ${product.Price - (product.Price * (product.Discount / 100))}.-
+                </div>
             </div>
         </div>
     </div>
